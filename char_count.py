9d223c6265c5ec0aefcd8d8041df44b6d9ddf1c0
Sony <62589156+sony7139@users.noreply.github.com>
2020-08-17 19:11:53 +0530

    Update char_count.py

--- char_count.py
+++ char_count.py
@@ -1,8 +1,11 @@
-str = input("Please enter a string")
-b = set(str)
-d = {}
-for char in b:
-    d[char] = str.count(char)
-sorted(d.values(),reverse=True)
-print(d)
+def most_freq(str):
+    b = set(str)
+    d = {}
+    for char in b:
+        d[char] = str.count(char)
+    sorted(d.values(),reverse=True)
+    print(d)
     
+str = input("Please enter a string")
+most_freq(str)
+
